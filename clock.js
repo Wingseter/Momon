e7b6938ca329a8b73735befa2a4075ed02673091
Wingseter <wingseter@gmail.com>
2020-08-25 09:43:00 +0900

    매초마다 시계 업데이트 및 표시 형식 변경

--- clock.js
+++ clock.js
@@ -6,11 +6,13 @@ function getTime(){
     const minute = date.getMinutes();
     const hours = date.getHours();
     const seconds = date.getSeconds();
-    clockTitle.innerText = `${hours}:${minute}:${seconds}`
+    clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${minute}:${seconds < 10 ? `0${seconds}` : seconds}`;
+
 }
 
 function init() {
     getTime();
+    setInterval(getTime, 1000)
 }
 
 init();
