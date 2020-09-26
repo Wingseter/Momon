6b886039569cd35e34de4609346b1e741d871671
Wingseter <wingseter@gmail.com>
2020-09-26 19:35:06 +0900

    날씨 불러오기 완성

--- weather.js
+++ weather.js
@@ -1,5 +1,18 @@
+const weather = document.querySelector(".js-weather");
+
 const COORDS = 'coords';
 
+function getWeather(lat, lon){
+    fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIKEY}&units=metric`
+    ).then(function(response){
+        return response.json();
+    }).then(function(json){
+        const temperature = json.main.temp;
+        const place = json.name;
+        weather.innerText = `${temperature} @ ${place}`
+    })
+}
 function saveCoords(coordsObj){
     localStorage.setItem(COORDS, JSON.stringify(coordsObj));
 }
@@ -13,6 +26,7 @@ function handleGeoSucces(position){
     };
 
     saveCoords(coordsObj);
+    getWeather(latitude, longitude);
 }
 
 function handleGeoError(){
@@ -23,11 +37,12 @@ function askForCoords(){
     navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError);
 }
 function loadCoods(){
-    const loaderCords = localStorage.getItem(COORDS);
-    if(loadCoods === null){
+    const loaderCoords = localStorage.getItem(COORDS);
+    if(loaderCoords === null){
         askForCoords();
     } else {
-
+        const parseCoords = JSON.parse(loaderCoords);
+        getWeather(parseCoords.latitude, parseCoords.longitude);
     }
 }
 function init() {
