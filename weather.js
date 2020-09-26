b8a2daaa1a6a36e9b41f7c4e61290454f921ada0
Wingseter <wingseter@gmail.com>
2020-09-26 18:13:01 +0900

    위치 정보 가져오기

---
+++ weather.js
@@ -0,0 +1,37 @@
+const COORDS = 'coords';
+
+function saveCoords(coordsObj){
+    localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSucces(position){
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const coordsObj = {
+        latitude,
+        longitude
+    };
+
+    saveCoords(coordsObj);
+}
+
+function handleGeoError(){
+    console.log("Cant access geo location");
+}
+
+function askForCoords(){
+    navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError);
+}
+function loadCoods(){
+    const loaderCords = localStorage.getItem(COORDS);
+    if(loadCoods === null){
+        askForCoords();
+    } else {
+
+    }
+}
+function init() {
+    loadCoods();
+}
+
+init();
